ab6e75dd5126ecbdc1ec82cc377298570737bd4b
ericrebillet <ericrebillet@gmail.com>
2025-09-08 12:44:28 +0630

    changes

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
-    console.log('jquery');
+
     // start back to top
-    $(".btn-bcktop").hide();
+    $(".btn-bckttop").hide();
     $(window).scroll(function(){
         var getscrolltop = $(this).scrollTop();
-        console.log(getscrolltop);
+        // console.log(getscrolltop);
 
-        if(getscrolltop >= 400){
-            $(".btn-bcktop").fadeIn(1000);
+        if(getscrolltop >= 370){
+            $(".btn-bckttop").fadeIn(1000);
         }else{
-            $(".btn-bcktop").fadeOut(1000);
+            $(".btn-bckttop").fadeOut(1000);
         }
     })
-})
+
+    // date time
+    const getyear = $('#getyear');
+    const getFullYear = new Date().getFullYear();
+    getyear.text(getFullYear);
+});
+
+function dropbtn(e) {
+    // console.log(e.target);
+    e.target.parentElement.nextElementSibling.classList.toggle('shows');
+}
